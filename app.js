6ee8edf6f093c88cd6a2339ade761b8b0b4259aa
sravanr788 <sravanr7882gmail.com>
2023-11-05 00:01:52 +0530

    game

---
+++ app.js
@@ -0,0 +1,5 @@
+let playbtn = document.getElementById("play-btn")
+
+playbtn.onclick = () =>{
+    window.location.href = "./intro.html"
+}
---
+++ instructions.js
@@ -0,0 +1,4 @@
+let closebtn = document.getElementById("close-btn")
+closebtn.onclick = ()=>{
+    window.location.href="./game.html"
+}
